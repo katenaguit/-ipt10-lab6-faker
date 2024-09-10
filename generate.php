c4d691e19bff96e71083cfe1432f44298cff7ea7
katenaguit <naguitkate15@gmail.com>
2024-09-10 19:15:57 +0800

    Update generate.php

--- generate.php
+++ generate.php
@@ -6,15 +6,12 @@ require_once 'Random.php';
 $random = new Random();
 $people = $random->generatePeople(300);
 
-// Open the file for writing
 $filename = 'persons.csv';
 $file = fopen($filename, 'w');
 
-// Write the header
 $header = array_keys($people[0]);
 fputcsv($file, $header);
 
-// Write the records
 foreach ($people as $person) {
     fputcsv($file, $person);
 }
